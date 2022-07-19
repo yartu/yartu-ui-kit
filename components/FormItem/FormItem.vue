38277a555da72de661b9c08dc555915cce3982d4
Ahmet <ahmet@yartu.io>
2022-07-19 17:41:41 +0300

    feat: added formItem component for all for items

---
+++ components/FormItem/FormItem.vue
@@ -0,0 +1,72 @@
+<script>
+
+import * as VALIDATIONS from './validations';
+
+export default {
+  name: 'formItem',
+  inject: ['yartuForm'],
+  data: () => ({
+    errors: [],
+  }),
+  props: {
+    rules: {
+      type: Array,
+      required: false,
+      default: () => [],
+    },
+    lazyOff: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+    ignoreValid: {
+      type: Boolean,
+      default: false,
+      required: false,
+    },
+  },
+  created() {
+    if (!this.ignoreValid) {
+      this.yartuForm(this);
+    }
+  },
+  methods: {
+    validate() {
+      this.errors = [];
+      const value = this.modelValue;
+      let valid = true;
+      for (let rule of this.rules) {
+        if (typeof rule === 'function') {
+          const isValid = rule(value);
+          if (isValid !== true) {
+            valid = isValid;
+            break;
+          }
+        } else if (typeof rule === 'string' && VALIDATIONS.default[rule]) {
+          const functions = VALIDATIONS.default[rule];
+          for (let ruleFunction of functions) {
+            const isValid = ruleFunction(value);
+            if (isValid !== true) {
+              valid = isValid;
+              break;
+            }
+          }
+        } else {
+          console.error('Yartu Ui Kit, form validation don\'t accept to: ', rule);
+        }
+      }
+      if (valid !== true) {
+        this.errors.push(valid);
+      }
+      return valid;
+    },
+  },
+  watch: {
+    modelValue(val) {
+      if (!this.lazyOff && !this.ignoreValid && this.rules && this.rules.length > 0) {
+        this.validate();
+      }
+    },
+  },
+};
+</script>
