b268f2635bb3cf1259132ee9934d1a9c0260a309
Ahmet <ahmet@yartu.io>
2022-07-21 18:38:11 +0300

    fix: input form register problem without form parent

--- components/FormItem/FormItem.vue
+++ components/FormItem/FormItem.vue
@@ -26,44 +26,48 @@ export default {
     },
   },
   created() {
-    if (!this.ignoreValid) {
+    try {
       this.yartuForm(this);
+    } catch {
+      // This input not have a form parent.
     }
   },
   methods: {
     validate() {
-      this.errors = [];
-      const value = this.modelValue;
-      let valid = true;
-      for (let rule of this.rules) {
-        if (typeof rule === 'function') {
-          const isValid = rule(value);
-          if (isValid !== true) {
-            valid = isValid;
-            break;
-          }
-        } else if (typeof rule === 'string' && VALIDATIONS.default[rule]) {
-          const functions = VALIDATIONS.default[rule];
-          for (let ruleFunction of functions) {
-            const isValid = ruleFunction(value);
+      if (!this.ignoreValid) {
+        this.errors = [];
+        const value = this.modelValue;
+        let valid = true;
+        for (let rule of this.rules) {
+          if (typeof rule === 'function') {
+            const isValid = rule(value);
             if (isValid !== true) {
               valid = isValid;
               break;
             }
+          } else if (typeof rule === 'string' && VALIDATIONS.default[rule]) {
+            const functions = VALIDATIONS.default[rule];
+            for (let ruleFunction of functions) {
+              const isValid = ruleFunction(value);
+              if (isValid !== true) {
+                valid = isValid;
+                break;
+              }
+            }
+          } else {
+            console.error('Yartu Ui Kit, form validation don\'t accept to: ', rule);
           }
-        } else {
-          console.error('Yartu Ui Kit, form validation don\'t accept to: ', rule);
         }
+        if (valid !== true) {
+          this.errors.push(valid);
+        }
+        return valid;
       }
-      if (valid !== true) {
-        this.errors.push(valid);
-      }
-      return valid;
     },
   },
   watch: {
     modelValue(val) {
-      if (!this.lazyOff && !this.ignoreValid && this.rules && this.rules.length > 0) {
+      if (!this.lazyOff && this.rules && this.rules.length > 0) {
         this.validate();
       }
     },
