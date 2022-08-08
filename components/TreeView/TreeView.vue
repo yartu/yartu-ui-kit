30e26a94fbaada6d23f199a5d862795241014a99
Aziz <aziz@yartu.io>
2022-08-08 12:38:45 +0300

    feat: new components init

---
+++ components/TreeView/TreeView.vue
@@ -0,0 +1,14 @@
+<template>
+  <!-- TODO:: create tree view @azizs4h -->
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: 'y-tree-view',
+  props: {},
+  computed: {},
+};
+</script>
+
+<style></style>
