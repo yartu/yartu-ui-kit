669152cf628dbfd38469d11e1b54b76ce6c55879
Aziz <aziz@yartu.io>
2022-08-09 17:33:18 +0300

    style: text-color and text size

--- components/ListBox/ListBox.vue
+++ components/ListBox/ListBox.vue
@@ -6,7 +6,7 @@
       type="button"
       @click="choose(item)"
       v-for="(item, index) in props.options"
-      class="block w-full border-b border-BORDER p-4 hover:bg-GREY-3 last:border-0 cursor-pointer"
+      class="block text-BLACK-02 text-sm w-full border-b border-BORDER p-4 hover:bg-GREY-3 last:border-0 cursor-pointer"
     >
       <slot name="template" :item="item">
         {{ item }}
