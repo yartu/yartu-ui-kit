20c2a516f1df3b0d4cb43c85c705de8f30545494
Ahmet <ahmet@yartu.io>
2022-08-25 00:43:28 +0300

    feat: Added Data Table

--- components/DataTable/DataTable.vue
+++ components/DataTable/DataTable.vue
@@ -1,53 +1,47 @@
 <template>
-  <div class="flex flex-col w-full">
-    <div class="overflow-x-auto">
-      <div class="inline-block min-w-full overflow-hidden align-middle">
-        <table :aria-describedby="ariaDesc" :class="tableClass">
-          <thead class="text-GREY-1 text-sm font-semibold">
-            <tr class="text-BLACK-2">
-              <th v-if="selectable" class="p-2.5 text-xs whitespace-nowrap font-semibold">
-                <Checkbox @click="selectAll" :checked="allChecked" :indeterminate="indeterminate"></Checkbox>
-              </th>
-              <th
-                v-for="header in headers"
-                :key="header.value"
-                class="p-2.5 text-GREY-1 text-xs whitespace-nowrap font-semibold"
-              >
-                <slot :name="`thead-${header.value}`" :item="header">
-                  {{ header.text }}
-                </slot>
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr
-              class="border-y border-BORDER text-BLACK-2 relative"
-              :class="{
-                selected: 'bg-LIGHTBLUE-4 before:content before:h-full before:w-0.5 before:bg-BLUE before:absolute before:inset-y-0 before:left-0',
-              }"
-              v-for="(body, index) in bodyData"
-              :key="index"
-            >
-              <td v-if="props.selectable" class="p-2.5 text-xs whitespace-nowrap font-semibold">
-                <Checkbox v-model="selectedList" :input-value="body.id"> </Checkbox>
-              </td>
-              <td
-                v-for="header in headers"
-                :key="header.value"
-                class="p-2.5 text-xs whitespace-nowrap font-semibold"
-                :class="{ 'text-right': header === 'actions' }"
-              >
-                <slot :name="`tbody-${header.value}`" :item="body">
-                  {{ body[header.value] || '-' }}
-                </slot>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    </div>
-  </div>
+  <SimpleTable>
+    <template #thead>
+      <tr class="text-BLACK-2">
+        <th v-if="props.selectable" class="p-2.5 text-xs whitespace-nowrap font-semibold">
+          <Checkbox @click="selectAll" :checked="allChecked" :indeterminate="indeterminate"></Checkbox>
+        </th>
+        <th
+          v-for="header in props.headers"
+          :key="header.value"
+          class="p-2.5 text-GREY-1 text-xs whitespace-nowrap font-semibold"
+        >
+          {{ header.text }}
+        </th>
+      </tr>
+    </template>
+    <template #tbody>
+      <tr
+        @click.stop="openDetail(item)"
+        class="border-y border-BORDER text-BLACK-2 relative hover:bg-LIGHTBLUE-4 cursor-pointer"
+        :class="{
+          'bg-LIGHTBLUE-4 before:w-0.5 before:bg-BLUE before:absolute before:inset-y-0 before:left-0': isActive(item),
+        }"
+        v-for="(item, index) in props.items"
+        :key="index"
+      >
+        <td v-if="props.selectable" class="p-2.5 text-xs w-6 whitespace-nowrap font-semibold">
+          <y-checkbox @click.native.stop='' v-model="selectedList" :input-value="item[inputValue]"></y-checkbox>
+        </td>
+        <td
+          v-for="header in props.headers"
+          :key="header.value"
+          class="p-2.5 text-xs whitespace-nowrap font-semibold max-w-[10rem] truncate"
+          :class="{ 'text-right': header === 'actions' }"
+        >
+          <slot :name="`y-table-${header.value}`" :item="item" :isActive="isActive">
+            {{ item[header.value] || '-' }}
+          </slot>
+        </td>
+      </tr>
+    </template>
+  </SimpleTable>
 </template>
+
 
 <script>
   export default {
@@ -58,61 +52,56 @@
 <script setup>
 
 import { ref, computed } from 'vue';
-import Checkbox from '../Checkbox';
+import { SimpleTable } from '../SimpleTable';
+import { Checkbox } from '../Checkbox';
 
 const props = defineProps({
-  ariaDesc: {
-    type: String,
-    default: '',
-  },
   selectable: {
     type: Boolean,
-    required: false,
-    default: () => false,
+    default: true,
+  },
+  items: {
+    type: Array,
+    required: true,
   },
   headers: {
     type: Array,
     required: false,
     default: () => [],
   },
-  bodyData: {
-    type: Array,
-    required: false,
-    default: () => [],
+  inputValue: {
+    type: String,
+    default: 'id',
   },
 });
 
-const emit = defineEmits(['selected']);
+const emit = defineEmits(['selected', 'choose']);
 
 const selectedList = ref([]);
 const allChecked = ref(false);
 
 const indeterminate = computed(() => {
   const selectedLength = selectedList.value.length;
-  const totalLength = props.contacts.length;
+  const totalLength = props.items.length;
   emit('selected', selectedList.value);
   return selectedLength > 0 && selectedLength !== totalLength;
 });
 
+const isActive = (item) => {
+  return selectedList.value.includes(item[props.inputValue]);
+};
+
+const openDetail = (item) => {
+  emit('choose', item);
+};
+
 const selectAll = () => {
   allChecked.value = !allChecked.value;
   if (allChecked.value) {
-    selectedList.value = props.contacts.map((i) => i.id);
+    selectedList.value = props.items.map((i) => i[props.inputValue]);
   } else {
     selectedList.value = [];
   }
   emit('selected', selectedList.value);
 };
-
-const tableClass = computed(() => {
-  return [
-    'border-collapse',
-    'table',
-    'min-w-full',
-    'items-center',
-    'text-sm',
-    'text-left',
-  ];
-});
-
 </script>
