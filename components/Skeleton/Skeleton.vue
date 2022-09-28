1bfb465bd9876b870f6ae7300dede160db8e29bd
Aziz <aziz@yartu.io>
2022-09-28 23:52:03 +0300

    refactor: skeleton email-content

--- components/Skeleton/Skeleton.vue
+++ components/Skeleton/Skeleton.vue
@@ -1,6 +1,10 @@
 <template>
   <div v-if="type.includes('title')" role="status" class="animate-pulse">
-      <div v-for="i in skeletonCount" :key="i" class="h-4 bg-gray-300 rounded-full dark:bg-gray-700 w-[240px] mt-0"></div>
+    <div
+      v-for="i in skeletonCount"
+      :key="i"
+      class="h-6 bg-BORDER rounded-full w-[240px] mt-0"
+    ></div>
   </div>
   <div
     v-else-if="type.includes('email-list')"
@@ -24,22 +28,71 @@
       </div>
     </div>
   </div>
-  <div v-else-if="type === 'email-content'" role="status" class="p-8 w-full rounded animate-pulse dark:border-gray-700">
-    <div class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-4"></div>
-    <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
-    <div class="flex items-center mt-4 space-x-3">
-        <svg class="w-14 h-14 text-gray-200 dark:text-gray-700" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z" clip-rule="evenodd"></path></svg>
-        <div>
-            <div class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-32 mb-2"></div>
-            <div class="w-48 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
+  <div
+    v-else-if="type === 'email-content'"
+    role="status"
+    class="w-full rounded animate-pulse"
+  >
+    <div class="flex flex-wrap items-center space-x-3 bg-LIGHTBLUE-6 px-6 py-4">
+      <div class="w-10 h-10 bg-BORDER rounded-full"></div>
+      <div class="flex-1 flex justify-between">
+        <div class="w-1/2 rounded-lg flex flex-col gap-2">
+          <div class="w-full h-4 rounded-lg bg-BORDER"></div>
+          <div class="w-1/3 h-4 rounded-lg bg-BORDER"></div>
         </div>
+        <div class="w-1/5 rounded-lg flex flex-col gap-2">
+          <div class="w-full h-3 rounded-lg bg-BORDER"></div>
+          <div class="w-1/2 h-3 rounded-lg bg-BORDER"></div>
+        </div>
+      </div>
     </div>
-    <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5 mt-8"></div>
-    <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
-    <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
-    <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
-    <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
-    <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
+    <div
+      class="flex flex-wrap gap-3 border-y border-BORDER px-6 py-2 items-center"
+    >
+      <template v-for="i in 8" :key="i">
+        <div class="w-8 h-8 bg-BORDER rounded-full"></div>
+        <div v-if="i % 3 === 0" class="h-6 w-px bg-BORDER"></div>
+      </template>
+    </div>
+
+    <div
+      v-for="i in 2"
+      :key="i"
+      :class="i % 2 === 0 ? 'flex-col-reverse' : ''"
+      role="status"
+      class="flex flex-col gap-2 animate-pulse duration-300 p-6"
+    >
+      <div class="flex items-center space-x-2 w-full">
+        <div class="h-4 bg-BORDER rounded-full w-32"></div>
+        <div class="h-4 bg-BORDER rounded-full w-24"></div>
+        <div class="h-4 bg-BORDER rounded-full w-full"></div>
+      </div>
+      <div class="flex items-center w-full space-x-2 max-w-[75%]">
+        <div class="h-4 bg-BORDER rounded-full w-full"></div>
+        <div class="h-4 bg-BORDER rounded-full w-full"></div>
+        <div class="h-4 bg-BORDER rounded-full w-24"></div>
+      </div>
+      <div class="flex items-center w-full space-x-2 max-w-[35%]">
+        <div class="h-4 bg-BORDER rounded-full w-full"></div>
+        <div class="h-4 bg-BORDER rounded-full w-80"></div>
+        <div class="h-4 bg-BORDER rounded-full w-full"></div>
+      </div>
+      <div class="flex items-center w-full space-x-2 max-w-[50%]">
+        <div class="h-4 bg-BORDER rounded-full w-full"></div>
+        <div class="h-4 bg-BORDER rounded-full w-full"></div>
+        <div class="h-4 bg-BORDER rounded-full w-24"></div>
+      </div>
+      <div class="flex items-center w-full space-x-2 max-w-[85%]">
+        <div class="h-4 bg-BORDER rounded-full w-32"></div>
+        <div class="h-4 bg-BORDER rounded-full w-24"></div>
+        <div class="h-4 bg-BORDER rounded-full w-full"></div>
+      </div>
+      <div class="flex items-center w-full space-x-2 max-w-[60%]">
+        <div class="h-4 bg-BORDER rounded-full w-full"></div>
+        <div class="h-4 bg-BORDER rounded-full w-80"></div>
+        <div class="h-4 bg-BORDER rounded-full w-full"></div>
+      </div>
+    </div>
   </div>
 </template>
 
