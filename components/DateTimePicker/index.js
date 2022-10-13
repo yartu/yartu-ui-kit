74316a0173647c84f43a8210a34a3a399d54bd6c
Ahmet <ahmet@yartu.io>
2022-10-13 10:32:50 +0300

    feat: add datetime picker

---
+++ components/DateTimePicker/index.js
@@ -0,0 +1,11 @@
+import DateTimePicker from './DateTimePicker.vue';
+
+const _DateTimePicker = {
+  install(Vue) {
+    Vue.component(DateTimePicker.name, DateTimePicker);
+  },
+};
+
+export default _DateTimePicker;
+
+export { DateTimePicker };
