093f94abf3fd76c7956243e94abd34023bbf491d
Aziz <aziz@yartu.io>
2022-12-13 11:39:44 +0300

    feat: varinant icon added and changed script to setup

--- components/Snackbar/Snackbar.vue
+++ components/Snackbar/Snackbar.vue
@@ -1,43 +1,34 @@
 <template>
   <div :class="snackbarContainer">
     <div>
-      <svg
-        width="36"
-        height="36"
-        viewBox="0 0 36 36"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <rect x="4.5" y="4.5" width="27" height="27" rx="13.5" fill="#1F2938" />
-        <path
-          fill-rule="evenodd"
-          clip-rule="evenodd"
-          d="M25.7957 13.1746C26.3897 13.7522 26.4029 14.7018 25.8254 15.2957L17.8026 23.5457C17.5171 23.8394 17.1239 24.0035 16.7144 23.9999C16.3048 23.9964 15.9145 23.8256 15.6341 23.5271L11.6568 19.2938C11.0896 18.69 11.1192 17.7407 11.7229 17.1735C12.3267 16.6062 13.276 16.6358 13.8432 17.2396L16.746 20.3292L23.6746 13.2043C24.2522 12.6103 25.2018 12.5971 25.7957 13.1746Z"
-          fill="white"
-        />
-        <rect
-          x="4.5"
-          y="4.5"
-          width="27"
-          height="27"
-          rx="13.5"
-          stroke="#1F2938"
-          stroke-width="3"
-        />
-      </svg>
+      <img
+        v-if="type === 'success'"
+        src="../../assets/images/svgs/icon-success-dark.svg"
+        alt="success"
+      />
+      <img
+        v-if="type === 'danger'"
+        src="../../assets/images/svgs/icon-close-dark.svg"
+        alt="error"
+      />
+      <img
+        v-if="type === 'warning'"
+        src="../../assets/images/svgs/icon-warning-dark.svg"
+        alt="error"
+      />
     </div>
     <slot></slot>
     <div class="flex flex-wrap gap-2 ml-auto min-w-fit">
       <button
         class="subtitle-4 !text-white hover:underline"
-        v-if="this.action"
-        @click="this.action.handler(close)"
+        v-if="action"
+        @click="action.handler(close)"
       >
         {{ action.text }}
       </button>
       <button
         v-if="closable"
-        @click="this.close"
+        @click="close"
         class="w-9 h-9 rounded-full hover:bg-BLACKOVERLAY flex items-center justify-center"
       >
         <svg
@@ -68,82 +59,83 @@
 <script>
 export default {
   name: 'y-snackbar',
-  props: {
-    bottom: {
-      type: Boolean,
-      default: false,
-    },
-    left: {
-      type: Boolean,
-      default: false,
-    },
-    right: {
-      type: Boolean,
-      default: false,
-    },
-    center: {
-      type: Boolean,
-      default: false,
-    },
-    type: {
-      type: String,
-      default: '',
-    },
-    closable: {
-      type: Boolean,
-      default: false,
-    },
-    duration: {
-      type: Number,
-      default: 4000,
-    },
-    iconColor: {
-      type: String,
-      required: false,
-      default: '#FFF',
-    },
-    finish: {
-      type: Function,
-      required: false,
-    },
-    action: {
-      type: Object,
-      required: false,
-    },
-  },
-  created() {
-    setTimeout(() => {
-      if (this.finish) {
-        this.finish();
-      }
-      this.$emit('close', this);
-    }, this.duration);
-  },
-  methods: {
-    close() {
-      this.$emit('close', this);
-    },
-  },
-  computed: {
-    snackbarContainer() {
-      return [
-        'flex',
-        'gap-3',
-        'items-center',
-        'w-full',
-        'px-4 py-3',
-        'text-white',
-        'rounded-xl',
-        'bg-BLACK-1',
-        'overflow-hidden',
-        'transition-all duration-300',
-        {
-          '!bg-RED': this.type === 'danger',
-          '!bg-GREEN': this.type === 'success',
-          '!bg-ORANGE': this.type === 'warning',
-        },
-      ];
-    },
-  },
 };
+</script>
+
+<script setup>
+import { computed } from 'vue';
+
+const emit = defineEmits(['close']);
+
+const props = defineProps({
+  bottom: {
+    type: Boolean,
+    default: false,
+  },
+  left: {
+    type: Boolean,
+    default: false,
+  },
+  right: {
+    type: Boolean,
+    default: false,
+  },
+  center: {
+    type: Boolean,
+    default: false,
+  },
+  type: {
+    type: String,
+    default: '',
+  },
+  closable: {
+    type: Boolean,
+    default: false,
+  },
+  duration: {
+    type: Number,
+    default: 4000,
+  },
+  iconColor: {
+    type: String,
+    required: false,
+    default: '#FFF',
+  },
+  finish: {
+    type: Function,
+    required: false,
+  },
+  action: {
+    type: Object,
+    required: false,
+  },
+});
+
+setTimeout(() => {
+  if (props.finish) {
+    props.finish();
+  }
+  emit('close', this);
+}, props.duration);
+
+const close = () => {
+  emit('close', this);
+};
+
+const snackbarContainer = computed(() => [
+  'flex',
+  'gap-3',
+  'items-center',
+  'w-full',
+  'px-4 py-3',
+  'text-white',
+  'rounded-xl',
+  'overflow-hidden',
+  'transition-all duration-300',
+  {
+    'bg-RED': props.type === 'danger',
+    'bg-GREEN': props.type === 'success' || props.type === '',
+    'bg-YELLOW': props.type === 'warning',
+  },
+]);
 </script>
