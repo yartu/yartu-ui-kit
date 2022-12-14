9896b32d44a8a00733f0104410847cf7be1ae81b
Aziz <aziz@yartu.io>
2022-12-14 19:06:24 +0300

    feat: timeline

---
+++ components/Timeline/index.js
@@ -0,0 +1,11 @@
+import Timeline from './Timeline.vue';
+
+const _Timeline = {
+  install(Vue) {
+    Vue.component(Timeline.name, Timeline);
+  },
+};
+
+export default _Timeline;
+
+export { Timeline };
