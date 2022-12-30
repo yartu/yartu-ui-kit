0cbc54e20c733e127169d0ddcb3b35db16ee9d84
Umut KENAR <umut.kenar@hotmail.com>
2022-12-30 14:24:51 +0300

    refactor: profile content skeleton change

--- components/Skeleton/ProfileContentSkeleton.vue
+++ components/Skeleton/ProfileContentSkeleton.vue
@@ -1,20 +1,24 @@
 <template>
-  <div role="status" class="w-full rounded animate-pulse">
-    <div class="flex flex-wrap items-center space-x-3 bg-LIGHTBLUE-6 px-6 py-4">
+  <div role="status" class="w-full animate-pulse">
+    <div
+      class="flex flex-wrap items-center rounded-lg gap-7 bg-LIGHTBLUE-6 p-4"
+    >
       <div class="flex-1 flex">
         <div class="w-20 h-20 bg-BORDER rounded-full"></div>
-        <div class="rounded-lg flex flex-col gap-2 ml-4">
-          <div class="w-full h-4 rounded-lg bg-BORDER"></div>
-          <div class="w-1/4 h-4 rounded-lg bg-BORDER"></div>
-        </div>
         <div class="w-1/5 rounded-lg flex flex-col gap-2">
           <div class="w-full h-3 rounded-lg bg-BORDER"></div>
           <div class="w-1/4 h-3 rounded-lg bg-BORDER"></div>
         </div>
       </div>
-      <div class="flex items-center w-full space-x-8 mt-6" v-for="i in 10">
-        <div class="h-7 bg-BORDER rounded-full w-full"></div>
-        <div class="h-7 bg-BORDER rounded-full w-full"></div>
+      <hr class="w-full" />
+      <div class="flex flex-col items-center w-full gap-7" v-for="i in 3">
+        <div class="flex gap-4 flex-wrap w-full">
+          <div
+            class="w-1/2-gap-4 h-5 rounded-full bg-BORDER"
+            v-for="i in 6"
+          ></div>
+        </div>
+        <hr class="w-full" v-if="i !== 3" />
       </div>
     </div>
   </div>
