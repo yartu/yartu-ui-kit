7e026fc9e59ca0325c6e3ab2658d8dc66c4f033f
Umut KENAR <umut.kenar@hotmail.com>
2023-01-12 13:37:56 +0300

    refactor: fix animate problem

--- components/Skeleton/NoteContentSkeleton.vue
+++ components/Skeleton/NoteContentSkeleton.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="w-full flex flex-col gap-5">
+  <div class="w-full flex flex-col gap-5 animate-pulse">
     <div class="w-full flex flex-col gap-2">
       <div class="h-6 bg-BORDER rounded-full w-1/2"></div>
       <div class="h-6 bg-BORDER rounded-full w-2/3"></div>
     </div>
-    <div class="flex flex-col w-full gap-2 animate-pulse">
+    <div class="flex flex-col w-full gap-2">
       <div class="h-4 bg-BORDER rounded-full w-2/3 mb-4"></div>
       <div
         v-for="i in skeletonCount"
