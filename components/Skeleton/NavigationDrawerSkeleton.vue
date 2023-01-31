b229fb7ed13e1ed8b9f892fc6af2ae71a2c5e8de
Umut KENAR <umut.kenar@hotmail.com>
2023-01-31 11:28:03 +0300

    feat: navigation drawer item skeleton added

---
+++ components/Skeleton/NavigationDrawerSkeleton.vue
@@ -0,0 +1,16 @@
+<template>
+  <div role="status" class="animate-pulse flex flex-col">
+    <div v-for="i in skeletonCount" :key="i" class="flex items-center gap-2 pl-1 py-2">
+      <div class="h-4 w-4 bg-BORDER rounded"></div>
+      <div class="h-3 w-20 bg-BORDER rounded-full"></div>
+    </div>
+  </div>
+</template>
+<script setup>
+const props = defineProps({
+  skeletonCount: {
+    type: Number,
+    required: true,
+  },
+});
+</script>
