489daa4de140333a7315bdef10b161020c6cc3d7
Umut KENAR <umut.kenar@hotmail.com>
2023-03-08 17:56:47 +0300

    refactor: change skeletons width for responsive

--- components/Skeleton/RepoFolderSkeleton.vue
+++ components/Skeleton/RepoFolderSkeleton.vue
@@ -2,7 +2,7 @@
   <div class="flex items-center w-full gap-4 flex-wrap animate-pulse">
     <div
       v-for="i in skeletonCount"
-      class="border border-BORDER animate-pulse px-4 py-3 flex gap-3 items-center rounded-lg"
+      class="border border-BORDER animate-pulse px-4 py-3 flex gap-3 items-center rounded-lg w-full 3xs:w-1/2-gap-4 sm:w-1/3-gap-4 xl:w-1/4-gap-4 3xl:w-1/5-gap-4"
     >
       <div class="w-8 h-8 bg-BORDER rounded-lg"></div>
 
--- components/Skeleton/StickyNoteSkeleton.vue
+++ components/Skeleton/StickyNoteSkeleton.vue
@@ -2,7 +2,7 @@
   <div class="flex items-center w-full gap-4 flex-wrap">
     <div
       v-for="i in skeletonCount"
-      class="w-1/5-gap-4 h-full bg-white rounded-lg p-2 animate-pulse border border-BORDER pt-10"
+      class="w-full 4xs:w-1/2-gap-6 xs:w-1/3-gap-6 lg:w-1/4-gap-6 2xl:w-1/5-gap-6 h-full bg-white rounded-lg p-2 animate-pulse border border-BORDER pt-10"
     >
       <div class="w-full h-64 bg-BORDER rounded-lg"></div>
     </div>
