798a9ef3ba95dcb3636f1eea7ab3288ff5c960ed
Umut KENAR <umut.kenar@hotmail.com>
2023-03-10 17:01:40 +0300

    refactor: scripts changed to composition api

--- components/InnerDropdown/InnerDropdown.vue
+++ components/InnerDropdown/InnerDropdown.vue
@@ -10,47 +10,52 @@
 
 <script>
 export default {
-  name: 'y-inner-dropdown',
-  props: {
-    bg: {
-      type: String,
-      default: '#ffffff',
-    },
-    left: {
-      type: Boolean,
-      default: false,
-    },
-  },
-  computed: {
-    bgStyle() {
-      return `background-color: ${this.bg};`;
-    },
-    containerClass() {
-      return [
-        'dropdown-inner-container',
-        'absolute z-1 top-0',
-        'hidden',
-        '-mt-5',
-        {
-          'pr-3 right-full': this.left,
-          'pl-3 left-full': !this.left,
-        },
-      ];
-    },
-    contentClass() {
-      return [
-        'shadow-1',
-        'relative z-1 top-full mt-3',
-        'py-2',
-        'bg-WHITE',
-        'text-sm font-semibold text-BLACK-2',
-        'flex flex-col max-w-[232px] w-[232px]',
-        'border-BORDER border rounded-lg',
-        'transition-all duration-300',
-      ];
-    },
-  },
+  name: "y-inner-dropdown",
 };
 </script>
 
+<script setup>
+import { computed } from "vue";
+
+const props = defineProps({
+  bg: {
+    type: String,
+    default: "#ffffff",
+  },
+  left: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const bgStyle = computed(() => {
+  return `background-color: ${props.bg};`;
+});
+
+const containerClass = computed(() => {
+  return [
+    "dropdown-inner-container",
+    "absolute z-1 top-0",
+    "hidden",
+    "-mt-5",
+    {
+      "pr-3 right-full": props.left,
+      "pl-3 left-full": !props.left,
+    },
+  ];
+});
+
+const contentClass = computed(() => {
+  return [
+    "shadow-1",
+    "relative z-1 top-full mt-3",
+    "py-2",
+    "bg-WHITE",
+    "text-sm font-semibold text-BLACK-2",
+    "flex flex-col max-w-[232px] w-[232px]",
+    "border-BORDER border rounded-lg",
+    "transition-all duration-300",
+  ];
+});
+</script>
 <style></style>
