b16cc85d3c36bd1ebdce671ea41b1c38f93b1f75
Umut KENAR <umut.kenar@hotmail.com>
2023-03-22 11:03:59 +0300

    fix: overflow problem

--- components/Skeleton/RepoFolderSkeleton.vue
+++ components/Skeleton/RepoFolderSkeleton.vue
@@ -4,11 +4,11 @@
       v-for="i in skeletonCount"
       class="border border-BORDER animate-pulse px-4 py-3 flex gap-3 items-center rounded-lg w-full 3xs:w-1/2-gap-4 sm:w-1/3-gap-4 xl:w-1/4-gap-4 3xl:w-1/5-gap-4"
     >
-      <div class="w-8 h-8 bg-BORDER rounded-lg"></div>
+      <div class="w-8 h-8 bg-BORDER rounded-lg shrink-0"></div>
 
-      <div class="flex flex-col gap-1">
-        <div class="w-28 h-4 bg-BORDER rounded-lg"></div>
-        <div class="w-48 h-3 bg-BORDER rounded-lg"></div>
+      <div class="flex flex-col gap-1 w-full">
+        <div class="w-1/2 h-3 bg-BORDER rounded-lg"></div>
+        <div class="w-4/5 h-2 bg-BORDER rounded-lg"></div>
       </div>
     </div>
   </div>
