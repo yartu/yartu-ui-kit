9ee20e4a2771d565eebcf6bfec3ca35e06222260
ramazan <ramazan@yartu.io>
2023-04-05 12:50:17 +0300

    style: w-full added for table item skeleton.

--- components/Skeleton/TableItemSkeleton.vue
+++ components/Skeleton/TableItemSkeleton.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col">
+  <div class="flex flex-col w-full">
     <div class="flex items-center w-full py-4 px-3 animate-pulse border-b border-BORDER" v-for="i in skeletonCount">
       <div class="w-1/2 flex items-center gap-3">
         <div class="h-6 w-6 bg-BORDER rounded flex-shrink-0"></div>
