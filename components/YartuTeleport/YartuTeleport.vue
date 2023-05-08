0763b3e6c9a878fe900582563b304774a8db42a1
Umut KENAR <umut.kenar@hotmail.com>
2023-05-08 09:44:52 +0300

    feat: teleport component added

---
+++ components/YartuTeleport/YartuTeleport.vue
@@ -0,0 +1,24 @@
+<template>
+  <teleport to="body">
+    <div ref="teleportedContent">
+      <slot></slot>
+    </div>
+  </teleport>
+</template>
+
+<script>
+export default {
+  name: 'y-teleport',
+};
+</script>
+
+<script setup>
+defineProps({
+  teleportTo: {
+    type: String,
+
+    default: 'body,',
+  },
+});
+</script>
+<style></style>
---
+++ components/YartuTeleport/index.js
@@ -0,0 +1,11 @@
+import YartuTeleport from "./YartuTeleport.vue";
+
+const _YartuTeleport = {
+  install(Vue) {
+    Vue.component(YartuTeleport.name, YartuTeleport);
+  }
+};
+
+export default _YartuTeleport;
+
+export { YartuTeleport };
