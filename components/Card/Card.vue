16f1e5f212fe5195ab5ba95754a3116810b6b3f4
Umut KENAR <umut.kenar@hotmail.com>
2023-07-13 16:10:01 +0300

    feat: add disable prop for card

--- components/Card/Card.vue
+++ components/Card/Card.vue
@@ -7,18 +7,31 @@
 <script>
 export default {
   name: 'y-card',
-  computed: {
-    containerClass() {
-      return [
-        'flex',
-        'border border-BORDER rounded-lg',
-        'hover:border-BLUE cursor-pointer',
-        'transition-all duration-300',
-        'hover:shadow-1',
-      ];
-    },
-  },
 };
+</script>
+
+<script setup>
+import { computed } from 'vue';
+
+const props = defineProps({
+  disabled: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const containerClass = computed(() => {
+  return [
+    'flex',
+    'border border-BORDER rounded-lg',
+    'transition-all duration-300',
+    'hover:shadow-1',
+    {
+      'hover:border-BLUE cursor-pointer': !props.disabled,
+      'bg-LIGHTBLUE-6 cursor-not-allowed' : props.disabled,
+    }
+  ];
+});
 </script>
 
 <style></style>
