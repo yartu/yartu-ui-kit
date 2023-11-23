21161b2a8a8992b4c6c1189e1f544a85140105c6
Oğuzhan Yıldız <oguzyildiz53@hotmail.com>
2023-11-23 16:54:56 +0300

    style: skeleton width edited

--- components/Skeleton/AdminCpuWidgetSkeleton.vue
+++ components/Skeleton/AdminCpuWidgetSkeleton.vue
@@ -1,6 +1,6 @@
 <template>
     <div v-for="i in skeletonCount" class="px-3 flex flex-col mb-3 w-full">
-          <div class="h-3 rounded-full w-1/3 mb-2 bg-BORDER"></div>
+          <div class="h-3 rounded-full w-[100px] mb-2 bg-BORDER"></div>
           <div class="w-full h-3 rounded-full  bg-BORDER"></div>
     </div>
   </template>
