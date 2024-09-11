89cdbecfc28d7c9bece3731f6ae6fe78a4056a60
Umut KENAR <umut.kenar@hotmail.com>
2024-09-11 16:32:20 +0300

    refactor: scripts changed to composition api

--- components/Button/Button.vue
+++ components/Button/Button.vue
@@ -35,128 +35,118 @@
 
 <script>
 export default {
-  name: "y-button",
-  props: {
-    disabled: {
-      type: Boolean,
-      default: false,
-    },
-    square: {
-      type: Boolean,
-      default: false,
-    },
-    circle: {
-      type: Boolean,
-      default: false,
-    },
-    primary: {
-      type: Boolean,
-      default: false,
-    },
-    secondary: {
-      type: Boolean,
-      default: false,
-    },
-    error: {
-      type: Boolean,
-      default: false,
-    },
-    success: {
-      type: Boolean,
-      default: false,
-    },
-    text: {
-      type: Boolean,
-      default: false,
-    },
-    tertiary: {
-      type: Boolean,
-      default: false,
-    },
-    loading: {
-      type: Boolean,
-      default: false,
-    },
-    icon: {
-      type: [String, Array],
-      default: "",
-    },
-    size: {
-      type: String,
-      default: "md",
-    },
-    app: {
-      type: Boolean,
-      default: false,
-    },
-    active: {
-      type: Boolean,
-      default: false,
-    },
-    spinnerBg: {
-      type: String,
-      default: "#f8fafd",
-    },
-    spinnerColor: {
-      type: String,
-      default: "#3663f2",
-    },
-    iconColor: {
-      type: String,
-      default: "none",
-    },
-  },
-  computed: {
-    buttonClass() {
-      return [
-        "font-semibold text-sm",
-        "rounded-lg",
-        "transition-all duration-300",
-        "inline-flex items-center justify-center",
-        "disabled:cursor-not-allowed",
-        {
-          "bg-BLUE text-white hover:bg-HOVER-BLUE disabled:bg-LIGHTBLUE-3":
-            this.primary,
-          "border border-BORDER bg-white text-BLUE hover:bg-LIGHTBLUE-5 disabled:bg-GREY-3":
-            this.secondary,
-          "bg-BLACK-2 text-white hover:bg-BLACK-3 disabled:bg-GREY-3 disabled:border-BORDER disabled:border":
-            this.tertiary,
-          "bg-RED text-white hover:bg-RED-3 disabled:opacity-50": this.error,
-          "bg-GREEN text-white hover:bg-GREEN-2 disabled:opacity-50":
-            this.success,
-          "text-BLUE hover:underline disabled:text-GREY-3 !p-0": this.text,
-
-          "h-10 py-2 px-7": this.size == "lg",
-          "h-9 py-2 px-3": this.size == "md",
-          "h-8 py-1 px-3": this.size == "sm",
-          "h-6 py-1 px-3": this.size == "xs",
-
-          "w-10 h-10 !p-0": this.square && this.size == "lg",
-          "w-9 h-9 !p-0": this.square && this.size == "md",
-          "w-8 h-8 !p-0": this.square && this.size == "sm",
-          "w-6 h-6 !p-0": this.square && this.size == "xs",
-
-          "w-10 h-10 !p-0 !rounded-full hover:bg-GREY-8":
-            this.circle && this.size == "lg",
-          "w-9 h-9 !p-0 !rounded-full hover:bg-GREY-8":
-            this.circle && this.size == "md",
-          "w-8 h-8 !p-0 !rounded-full hover:bg-GREY-8":
-            this.circle && this.size == "sm",
-          "w-6 h-6 !p-0 !rounded-full hover:bg-GREY-8":
-            this.circle && this.size == "xs",
-          "w-4 h-4 !p-0 !rounded-full hover:bg-GREY-8":
-            this.circle && this.size == "2xs",
-
-          "bg-LIGHTBLUE-3 hover:bg-LIGHTBLUE-3": this.loading && this.primary,
-          "bg-GREY-3 hover:bg-GREY-3":
-            this.loading && (this.secondary || this.tertiary),
-          "transition-none outline-LIGHTBLUE-5 outline-2 hover:outline":
-            this.app,
-          "bg-LIGHTBLUE-5 outline-2 border-2 outline border-BLUE outline-LIGHTBLUE-5":
-            this.app && this.active,
-        },
-      ];
-    },
-  },
+  name: 'y-button',
 };
+</script>
+
+<script setup>
+import { computed } from 'vue';
+
+const props = defineProps({
+  disabled: {
+    type: Boolean,
+    default: false,
+  },
+  square: {
+    type: Boolean,
+    default: false,
+  },
+  circle: {
+    type: Boolean,
+    default: false,
+  },
+  primary: {
+    type: Boolean,
+    default: false,
+  },
+  secondary: {
+    type: Boolean,
+    default: false,
+  },
+  error: {
+    type: Boolean,
+    default: false,
+  },
+  success: {
+    type: Boolean,
+    default: false,
+  },
+  text: {
+    type: Boolean,
+    default: false,
+  },
+  tertiary: {
+    type: Boolean,
+    default: false,
+  },
+  loading: {
+    type: Boolean,
+    default: false,
+  },
+  icon: {
+    type: [String, Array],
+    default: '',
+  },
+  size: {
+    type: String,
+    default: 'md',
+  },
+  app: {
+    type: Boolean,
+    default: false,
+  },
+  active: {
+    type: Boolean,
+    default: false,
+  },
+  spinnerBg: {
+    type: String,
+    default: '#f8fafd',
+  },
+  spinnerColor: {
+    type: String,
+    default: '#3663f2',
+  },
+  iconColor: {
+    type: String,
+    default: 'none',
+  },
+});
+
+const buttonClass = computed(() => [
+  'font-semibold text-sm',
+  'rounded-lg',
+  'transition-all duration-300',
+  'inline-flex items-center justify-center',
+  'disabled:cursor-not-allowed',
+  {
+    'bg-BLUE text-white hover:bg-HOVER-BLUE disabled:bg-LIGHTBLUE-3': props.primary,
+    'border border-BORDER bg-white text-BLUE hover:bg-LIGHTBLUE-5 disabled:bg-GREY-3': props.secondary,
+    'bg-BLACK-2 text-white hover:bg-BLACK-3 disabled:bg-GREY-3 disabled:border-BORDER disabled:border': props.tertiary,
+    'bg-RED text-white hover:bg-RED-3 disabled:opacity-50': props.error,
+    'bg-GREEN text-white hover:bg-GREEN-2 disabled:opacity-50': props.success,
+    'text-BLUE hover:underline disabled:text-GREY-3 !p-0': props.text,
+
+    'h-10 py-2 px-7': props.size == 'lg',
+    'h-9 py-2 px-3': props.size == 'md',
+    'h-8 py-1 px-3': props.size == 'sm',
+    'h-6 py-1 px-3': props.size == 'xs',
+
+    'w-10 h-10 !p-0': props.square && props.size == 'lg',
+    'w-9 h-9 !p-0': props.square && props.size == 'md',
+    'w-8 h-8 !p-0': props.square && props.size == 'sm',
+    'w-6 h-6 !p-0': props.square && props.size == 'xs',
+
+    'w-10 h-10 !p-0 !rounded-full hover:bg-GREY-8': props.circle && props.size == 'lg',
+    'w-9 h-9 !p-0 !rounded-full hover:bg-GREY-8': props.circle && props.size == 'md',
+    'w-8 h-8 !p-0 !rounded-full hover:bg-GREY-8': props.circle && props.size == 'sm',
+    'w-6 h-6 !p-0 !rounded-full hover:bg-GREY-8': props.circle && props.size == 'xs',
+    'w-4 h-4 !p-0 !rounded-full hover:bg-GREY-8': props.circle && props.size == '2xs',
+
+    'bg-LIGHTBLUE-3 hover:bg-LIGHTBLUE-3': props.loading && props.primary,
+    'bg-GREY-3 hover:bg-GREY-3': props.loading && (props.secondary || props.tertiary),
+    'transition-none outline-LIGHTBLUE-5 outline-2 hover:outline': props.app,
+    'bg-LIGHTBLUE-5 outline-2 border-2 outline border-BLUE outline-LIGHTBLUE-5': props.app && props.active,
+  },
+]);
 </script>
